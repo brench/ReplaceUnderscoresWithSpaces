3616ee1d4a3ff4ab9b533e61f23cb66a879a3b86
brench12 <renchdev@gmail.com>
2019-12-03 11:44:09 -0500

    Initial commit

---
+++ ReplaceSpacesWithUnderscores/ClipBoardMonitor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReplaceSpacesWithUnderscores
+{
+
+    // https://stackoverflow.com/questions/31258905/how-to-monitor-clipboard-changes-in-c-sharp-to-capture-only-hyperlinks
+
+    public class ClipBoardMonitor : NativeWindow
+    {
+
+        private const int WM_DESTROY = 0x2;
+        private const int WM_DRAWCLIPBOARD = 0x308;
+        private const int WM_CHANGECBCHAIN = 0x30d;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+        [DllImport("user32.dll")]
+        private static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        public event NewUrlHandler NewUrl;
+        public delegate void NewUrlHandler(string txt);
+
+        private IntPtr NextClipBoardViewerHandle;
+
+        public ClipBoardMonitor()
+        {
+            this.CreateHandle(new CreateParams());
+            this.NextClipBoardViewerHandle = SetClipboardViewer(this.Handle);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    if (Clipboard.ContainsText())
+                    {
+                        string txt = Clipboard.GetText();
+                        if (this.NewUrl != null && this.IsValidUrl(txt))
+                        {
+                            this.NewUrl(txt);
+                        }
+                    }
+                    SendMessage(this.NextClipBoardViewerHandle, m.Msg, m.WParam, m.LParam);
+
+                    break;
+
+                case WM_CHANGECBCHAIN:
+                    if (m.WParam.Equals(this.NextClipBoardViewerHandle))
+                    {
+                        this.NextClipBoardViewerHandle = m.LParam;
+                    }
+                    else if (!this.NextClipBoardViewerHandle.Equals(IntPtr.Zero))
+                    {
+                        SendMessage(this.NextClipBoardViewerHandle, m.Msg, m.WParam, m.LParam);
+                    }
+                    break;
+
+                case WM_DESTROY:
+                    ChangeClipboardChain(this.Handle, this.NextClipBoardViewerHandle);
+                    break;
+
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private bool IsValidUrl(string txt)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(txt, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+    }
+}
---
+++ ReplaceSpacesWithUnderscores/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReplaceSpacesWithUnderscores
+{
+    public partial class Form1 : Form
+    {
+        // https://www.fluxbytes.com/csharp/how-to-monitor-for-clipboard-changes-using-addclipboardformatlistener/
+
+        // See http://msdn.microsoft.com/en-us/library/ms649021%28v=vs.85%29.aspx
+        public const int WM_CLIPBOARDUPDATE = 0x031D;
+        public static IntPtr HWND_MESSAGE = new IntPtr(-3);
+
+        // See http://msdn.microsoft.com/en-us/library/ms632599%28VS.85%29.aspx#message_only
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        // See http://msdn.microsoft.com/en-us/library/ms633541%28v=vs.85%29.aspx
+        // See http://msdn.microsoft.com/en-us/library/ms649033%28VS.85%29.aspx
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        //private ClipBoardMonitor cbm = null;
+
+        /// <summary>
+        /// Occurs when the contents of the clipboard is updated.
+        /// </summary>
+        //public static event EventHandler ClipboardUpdate;
+
+        private string _userStoryString = null;
+        private string _taskString = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //NativeMethods.SetParent(Handle, NativeMethods.HWND_MESSAGE);
+            //NativeMethods.AddClipboardFormatListener(Handle);
+
+            AddClipboardFormatListener(this.Handle);
+            
+
+            //cbm = new ClipBoardMonitor();
+            //cbm.NewUrl += Cbm_NewUrl;
+
+            //ClipboardNotification.ClipboardUpdate += ClipboardNotification_ClipboardUpdate;
+        }
+
+        //private void ClipboardNotification_ClipboardUpdate(object sender, EventArgs e)
+        //{
+        //    textBox1.Text = Clipboard.GetText();
+        //}
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                //OnClipboardUpdate(null);
+                IDataObject iData = Clipboard.GetDataObject();      // Clipboard's data.
+
+                /* Depending on the clipboard's current data format we can process the data differently. 
+                 * Feel free to add more checks if you want to process more formats. */
+                if (iData.GetDataPresent(DataFormats.Text))
+                {
+                    string text = (string)iData.GetData(DataFormats.Text);
+                    // do something with it
+                    if (text.Contains("User Story"))
+                    {
+                        _userStoryString = text.Replace("User Story", string.Empty).Trim();
+                        _userStoryString = _userStoryString.Insert(0, "feature/");
+                        _userStoryString = _userStoryString.Replace(":", string.Empty);
+                        _userStoryString = _userStoryString.Replace(" ", "_");
+                    }
+                    else if (_userStoryString != null) // Task
+                    {
+                        _taskString = text.Replace("Task", string.Empty).Trim();
+                        _taskString = _taskString.Replace(":", string.Empty);
+                        _taskString = _taskString.Replace(" ", "_");
+                        var stringForClipboard = $"{_userStoryString}.{_taskString}";
+                        _userStoryString = null;
+                        _taskString = null;
+                        Clipboard.SetText(stringForClipboard);
+                    }
+
+
+                }
+                else if (iData.GetDataPresent(DataFormats.Bitmap))
+                {
+                    Bitmap image = (Bitmap)iData.GetData(DataFormats.Bitmap);
+                    // do something with it
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        ///// <summary>
+        ///// Raises the <see cref="ClipboardUpdate"/> event.
+        ///// </summary>
+        ///// <param name="e">Event arguments for the event.</param>
+        //private static void OnClipboardUpdate(EventArgs e)
+        //{
+        //    var handler = ClipboardUpdate;
+        //    if (handler != null)
+        //    {
+        //        handler(null, e);
+        //    }
+        //}
+
+        private void Cbm_NewUrl(string txt)
+        {
+            this.textBox1.Text = txt;
+        }
+
+        private void ChangeClipboardText()
+        {
+            var clipboardText = Clipboard.GetText();
+            
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var text = textBox1.Text;
+
+            if (Clipboard.GetText().Contains("feature/"))
+            {
+
+            }
+
+            if (text.Contains("User Story"))
+            {
+                text = textBox1.Text.Replace("User Story", string.Empty).Trim();
+                text = text.Insert(0, "feature/");
+            }
+            else // Task
+            {
+                text = text.Replace("Task", string.Empty);
+            }
+
+            text = text.Replace(":", string.Empty);
+            text = text.Replace(" ", "_");
+
+            Clipboard.SetText(text.Trim());
+            textBox1.SelectAll();
+        }
+    }
+
+    //internal static class NativeMethods
+    //{
+    //    // See http://msdn.microsoft.com/en-us/library/ms649021%28v=vs.85%29.aspx
+    //    public const int WM_CLIPBOARDUPDATE = 0x031D;
+    //    public static IntPtr HWND_MESSAGE = new IntPtr(-3);
+
+    //    // See http://msdn.microsoft.com/en-us/library/ms632599%28VS.85%29.aspx#message_only
+    //    [DllImport("user32.dll", SetLastError = true)]
+    //    [return: MarshalAs(UnmanagedType.Bool)]
+    //    public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+    //    // See http://msdn.microsoft.com/en-us/library/ms633541%28v=vs.85%29.aspx
+    //    // See http://msdn.microsoft.com/en-us/library/ms649033%28VS.85%29.aspx
+    //    [DllImport("user32.dll", SetLastError = true)]
+    //    public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+    //}
+}
